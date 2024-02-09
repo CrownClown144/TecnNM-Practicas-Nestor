4a70ead2bcc3c720c7979c89db23559c00795369
Nestor Flores <lrs20110456@purisima.tecnm.mx>
2024-02-09 11:07:04 -0600

    Archivos completos

---
+++ TecNM.Practica1.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TecNM.Practica1.Core.Entities;
+using TecNM.Practica1.Core.Managers;
+using TecNM.Practica1.Core.Services;
+
+namespace TecNM.Practica1.App;
+
+public static class Program{
+    public static void Main(string[] args) {
+        float weight = 0;
+
+        System.Console.WriteLine("Por favor, introduce tu peso");
+        Single.TryParse(System.Console.ReadLine(), out weight);
+
+        var person = new Person{Weigth = weight};
+
+        var service = new MarteServices();
+        var manager =  new MarteManager(service);
+
+        Marte marte = manager.GetMarte(person);
+
+         System.Console.WriteLine($"Tu peso de:  {weight}kg en la tierra es equivalente a: {marte.PesoMarte}kg en marte");
+    }
+}
